6bd2676e5eb2032cc297d7653f93af5e1f9272c0
Ormeline <ormelineluyengi@outlook.com>
2023-04-21 14:38:08 +0100

    Testing ParentClass- Challenge v2- update to spec file

--- Unit-Testing-Parent-Child-Classes/spec/task_list_spec.rb
+++ Unit-Testing-Parent-Child-Classes/spec/task_list_spec.rb
@@ -2,47 +2,24 @@ require "task_list"
 
 RSpec.describe TaskList do
   it "initially has an empty list" do
-    task_list = FakeAll.new
+    task_list = double("task_list", all:[])
     expect(task_list.all).to eq([])
   end
 
   it " is initially not all complete" do
-    task_list = FakeAllCompleteTask.new
-    expect(task_list.all_complete?).to eq(true)
+    task_list = double("task_list", all_complete?: false, complete?: false)
+    expect(task_list.all_complete?).to eq(false)
   end
 
   it "checks if tasks are all complete" do
-    task_1 = FakeAllCompleteTask.new
-    task_2 = FakeAllInCompleteTask.new
+    task_1 = double("task_1", all_complete?: true)
+    task_2 = double("task_2", all_complete?: true)
+    task_1 = double("task_1", complete?: true)
+    task_2 = double("task_2", complete?: true)
     task_list = TaskList.new
     task_list.add(task_1)
     task_list.add(task_2)
-    expect(task_list.all_complete?).to eq(false)
+    expect(task_list.all_complete?).to eq(true)
   end
 end
 
-class FakeAll
-  def all
-    []
-  end
-end
-
-class FakeAllCompleteTask
-    def all_complete?
-        true
-    end
-
-  def complete?
-    true
-  end
-end
-
-class FakeAllInCompleteTask
-    def all_complete?
-      false
-    end
-
-    def complete?
-        false
-    end
-end
